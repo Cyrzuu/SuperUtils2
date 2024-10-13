62f3ea6fd24d77378cc7c2b9461058799c92761c
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-10-13 15:50:56 +0200

    1.0.7

--- src/main/java/me/cyrzu/git/superutils2/bridges/map/MapBridge.java
+++ src/main/java/me/cyrzu/git/superutils2/bridges/map/MapBridge.java
@@ -3,14 +3,16 @@ package me.cyrzu.git.superutils2.bridges.map;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public interface MapBridge extends Map<String, Object> {
 
     @NotNull
     MapBridge EMPTY_MAP_BRIDGE = new EmptyMapBridge();
+
+    static MapBridge create() {
+        return new Dynamic();
+    }
 
     @NotNull
     MapBridge getMapBridge(@NotNull String path);
@@ -60,6 +62,191 @@ public interface MapBridge extends Map<String, Object> {
     @Override
     default Collection<Object> values() {
         throw new UnsupportedOperationException("This operation is not supported on this map.");
+    }
+
+    class Dynamic implements MapBridge {
+
+        private final Map<String, Object> map = new LinkedHashMap<>();
+
+        private Dynamic() {
+        }
+
+        @Override
+        public int size() {
+            return map.size();
+        }
+
+        @Nullable
+        @Override
+        public Object get(Object key) {
+            if (key instanceof String) {
+                String[] parts = ((String) key).split("\\.");
+                return getValueByPath(parts, 0);
+            }
+            return null;
+        }
+
+        private Object getValueByPath(String[] parts, int index) {
+            if (index >= parts.length) {
+                return null;
+            }
+
+            String currentKey = parts[index];
+            Object next = map.get(currentKey);
+
+            if (index == parts.length - 1) {
+                return next;
+            }
+
+            if (next instanceof Dynamic dynamic) {
+                return dynamic.getValueByPath(parts, index + 1);
+            }
+
+            return null;
+        }
+
+        @Override
+        public Object put(String key, Object value) {
+            if (key != null) {
+                String[] parts = key.split("\\.");
+                putValueByPath(parts, 0, value);
+            }
+            return value;
+        }
+
+        private void putValueByPath(String[] parts, int index, Object value) {
+            if (index >= parts.length) {
+                return;
+            }
+
+            String currentKey = parts[index];
+            if (index == parts.length - 1) {
+                map.put(currentKey, value);
+            } else {
+                Object next = map.get(currentKey);
+
+                if (!(next instanceof Dynamic)) {
+                    next = new Dynamic();
+                    map.put(currentKey, next);
+                }
+
+                ((Dynamic) next).putValueByPath(parts, index + 1, value);
+            }
+        }
+
+        @NotNull
+        @Override
+        public Set<String> keySet() {
+            return map.keySet();
+        }
+
+        @NotNull
+        @Override
+        public Set<Entry<String, Object>> entrySet() {
+            return map.entrySet();
+        }
+
+        @NotNull
+        @Override
+        public MapBridge getMapBridge(@NotNull String path) {
+            String[] parts = path.split("\\.");
+            return this.getOrCreateNestedMap(parts, 0);
+        }
+
+        private MapBridge getOrCreateNestedMap(String[] parts, int index) {
+            if (index >= parts.length) {
+                return this;
+            }
+
+            String currentKey = parts[index];
+            Object next = map.get(currentKey);
+
+            if (next instanceof Dynamic dynamic) {
+                // Jeśli istnieje mapBridge dla obecnego klucza, przechodzimy dalej
+                return dynamic.getOrCreateNestedMap(parts, index + 1);
+            } else {
+                // Jeśli nie istnieje mapBridge, tworzymy nowy i idziemy dalej
+                Dynamic newBridge = new Dynamic();
+                map.put(currentKey, newBridge);
+                return newBridge.getOrCreateNestedMap(parts, index + 1);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return map.toString();
+        }
+    }
+
+    class EmptyMapBridge implements MapBridge {
+
+        private EmptyMapBridge() {
+        }
+
+        @Override
+        public @NotNull MapBridge getMapBridge(@NotNull String path) {
+            return this;
+        }
+
+        @Override
+        public int size() {
+            return 0;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return false;
+        }
+
+        @Override
+        public boolean containsKey(Object key) {
+            return false;
+        }
+
+        @Override
+        public boolean containsValue(Object value) {
+            return false;
+        }
+
+        @Override
+        public Object get(Object key) {
+            return null;
+        }
+
+        @Override
+        public @Nullable Object put(String key, Object value) {
+            return null;
+        }
+
+        @Override
+        public Object remove(Object key) {
+            return null;
+        }
+
+        @Override
+        public void putAll(@NotNull Map<? extends String, ?> m) {
+
+        }
+
+        @Override
+        public void clear() {
+        }
+
+        @Override
+        public @NotNull Set<String> keySet() {
+            return Set.of();
+        }
+
+        @Override
+        public @NotNull Collection<Object> values() {
+            return List.of();
+        }
+
+        @Override
+        public @NotNull Set<Entry<String, Object>> entrySet() {
+            return Set.of();
+        }
+
     }
 
 }
