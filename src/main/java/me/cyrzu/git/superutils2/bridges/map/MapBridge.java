c26d0407d11d247e77b14939b2f1281b8aafed36
Cyrzuu <67297610+Cyrzuu@users.noreply.github.com>
2024-10-13 20:52:09 +0200

    1.0.8

--- src/main/java/me/cyrzu/git/superutils2/bridges/map/MapBridge.java
+++ src/main/java/me/cyrzu/git/superutils2/bridges/map/MapBridge.java
@@ -68,7 +68,7 @@ public interface MapBridge extends Map<String, Object> {
 
         private final Map<String, Object> map = new LinkedHashMap<>();
 
-        private Dynamic() {
+        Dynamic() {
         }
 
         @Override
